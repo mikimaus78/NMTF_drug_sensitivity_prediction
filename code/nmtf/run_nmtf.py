f59d5aaa93f344cda8605ab0eb472d28cb8b223f
ThomasBrouwer <tab43@cam.ac.uk>
2015-04-09 12:22:27 +0100

    Observation: running NMF on the Sanger dataset gives nearly identical reconstructions, regardless of the value of K. The same applies to NMTF - identical reconstruction to NMF (up to ~10 digits), and little to no variation due to choice of K and L.

--- code/nmtf/run_nmtf.py
+++ code/nmtf/run_nmtf.py
@@ -1,35 +1,41 @@
 """
 Load in the drug sensitivity dataset.
 Take out 10% of the values.
-Use Non-Negative Matrix Factorisation (I-divergence implementation) to 
+Use Non-Negative Matrix Tri-Factorisation (I-divergence implementation) to 
 predict the missing values.
 
-Performance: MSE of ~3.1 on X_min dataset.
+Performance (MSE) on X_min dataset:
+Standard initialisation:
+- K=L=1  -> 3.1441809643762459
+- K=L=10 -> 3.1447522717912841
+- K=L=50 -> 
+Kmeans initialisation:
+TODO: this
 """
 
 import sys
 sys.path.append("/home/thomas/Documenten/PhD/")#("/home/tab43/Documents/Projects/libraries/")
 import numpy, itertools, matplotlib.pyplot as plt
-from nmf_i_div.code.nmf import NMF
+from nmtf_i_div.code.nmtf import NMTF
 import ml_helpers.code.mask as mask
 import ml_helpers.code.statistics as statistics
 from NMTF_drug_sensitivity_prediction.code.helpers.load_data import load_Sanger
 
 
 # Try each of the values for k in the list <k_values>, and return the performances.
-def try_different_k(X,M,no_folds,K_values,seed,iterations,updates):
+def try_different_k(X,M,no_folds,K_L_values,seed,iterations,updates):
     mean_MSEs = []
     mean_i_divs = []
-    for K in K_values:
-        print "Running cross-validation with value K=%s." % K
-        MSEs,i_divs = run_cross_validation(X,M,no_folds,K,seed,iterations,updates)
+    for K,L in K_L_values:
+        print "Running cross-validation with value K=%s, L=%s." % (K,L)
+        MSEs,i_divs = run_cross_validation(X,M,no_folds,K,L,seed,iterations,updates)
         mean_MSEs.append(sum(MSEs) / float(len(MSEs)))
         mean_i_divs.append(sum(i_divs) / float(len(i_divs)))
         
     return (mean_MSEs,mean_i_divs)
     
-# Run NMF on different folds    
-def run_cross_validation(X,M,no_folds,K,seed,iterations,updates):
+# Run NMTF on different folds    
+def run_cross_validation(X,M,no_folds,K,L,seed,iterations,updates):
     (I,J) = M.shape
     folds_M = mask.compute_folds(I,J,no_folds,seed,M)
     Ms = mask.compute_Ms(folds_M)
@@ -43,7 +49,7 @@ def run_cross_validation(X,M,no_folds,K,seed,iterations,updates):
         M_test = folds_M[fold]
         assert numpy.array_equal(M_training+M_test, M)
         
-        (MSE,i_div) = run_NMF(X,M_training,M_test,K,iterations,updates)
+        (MSE,i_div) = run_NMTF(X,M_training,M_test,K,L,iterations,updates)
         MSEs.append(MSE)
         i_divs.append(i_div)
         
@@ -65,12 +71,12 @@ def assert_no_empty_rows_columns(Ms):
             assert len(omega_j) != 0, "Fully unobserved column in M, column %s." % j
     
 
-# Run NMF on the training data X, with known values <M_training> and test entries <M_test>.
-def run_NMF(X,M_training,M_test,K,iterations,updates):
-    nmf = NMF(X,M_training,K)
-    nmf.initialise()
-    nmf.run(iterations,updates,True)
-    X_pred = nmf.V_pred
+# Run NMTF on the training data X, with known values <M_training> and test entries <M_test>.
+def run_NMTF(X,M_training,M_test,K,L,iterations,updates):
+    nmtf = NMTF(X,M_training,K,L)
+    nmtf.initialise()
+    nmtf.run(iterations,updates)
+    X_pred = nmtf.R_pred
     
     # Calculate MSE of predictions.
     MSE = statistics.MSE(X,X_pred,M_test)
@@ -88,28 +94,31 @@ if __name__ == "__main__":
     X_filtered = numpy.array([[0 if v < 0 else v for v in row] for row in X])
     M_filtered = numpy.array([[0 if (v < 0 or not M[i][j]) else 1 for j,v in enumerate(row)] for i,row in enumerate(X)])
     
-    """ Run NMF cross-validation for different K's """
+    """ Run NMTF cross-validation for different K's """
     no_folds = 5
     seed = 0
     iterations = 5
     updates = 1
-    Ks = range(1,50+1)
-    K = 1
-    print run_cross_validation(X_min,M,no_folds,K,seed,iterations,updates)
-    #print run_cross_validation(X_filtered,M_filtered,no_folds,K,seed,iterations,updates)
     
-    '''
-    (MSEs,i_divs) = try_different_k(X_min,M,no_folds,Ks,seed,iterations,updates)
-    (MSEs_filtered,i_divs_filtered) = try_different_k(X_filtered,M_filtered,no_folds,Ks,seed,iterations,updates)
+    K = 1
+    L = 1
+    print run_cross_validation(X_min,M,no_folds,K,L,seed,iterations,updates)
+    #print run_cross_validation(X_filtered,M_filtered,no_folds,10,seed,iterations,updates)
+    
+    #K_L_values = itertools.product(range(1,2+1),range(1,2+1))
+    #(MSEs,i_divs) = try_different_k(X_min,M,no_folds,K_L_values,seed,iterations,updates)
+    #(MSEs_filtered,i_divs_filtered) = try_different_k(X_filtered,M_filtered,no_folds,K_L_values,seed,iterations,updates)
+    #print MSEs,i_divs   
     
     """
     Note: MSE and i_div for X_min and X_filtered stay roughly the same for 
           different values of K (MSE of ~3.15 and ~2.1 resp.).
     """    
     
+    '''
     """ Plot the performances with varying K's """
     fig = plt.figure(0)
-    plt.plot(Ks,MSEs)
+    plt.plot(K_L_values,MSEs)
     plt.title("MSE for X_min")
     
     fig = plt.figure(1)
